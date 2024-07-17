917e4975702577630478909d6f8ff56e4ca35b5f
Peyman Shahidi <shahidi.peyman96@gmail.com>
2024-07-17 04:13:49 -0400

    updated machinery
    new tools allow adding and comparing multiple methods of DAG production

---
+++ edsl_rubin/peyman_codes/functions.py
@@ -0,0 +1,170 @@
+# Functions
+
+# Pick occupation
+def pick_occupation(occupation):
+    if occupation == 'travelAgents':
+        GPT_input_occupation = 'travel agents'
+        plot_title_occupation = 'Travel Agents'
+        occupation_code = '41-3041'
+    elif occupation == 'insuranceUnderwriters':
+        GPT_input_occupation = 'insurance underwriters'
+        plot_title_occupation = 'Insurance Underwriters'
+        occupation_code = '13-2053'
+    
+    occupation_folder = f'{data_path}/daily_tasks_occupations_analysis/{occupation}'
+    return GPT_input_occupation, plot_title_occupation, occupation_code, occupation_folder
+
+
+
+# Hacky way of fixing "Sink" node
+def add_sink_node(dag_matrix, occupation):
+    n = dag_matrix.shape[0]
+    tasks = dag_matrix.columns.tolist()
+    
+    if '"Sink"' in tasks:
+        return dag_matrix
+    elif occupation == 'travelAgents':
+        dag_matrix.loc['"Sink"'] = [0] * n
+        dag_matrix['"Sink"'] = [0, 0, 0, 0, 0, 0, 1, 1, 0]
+    elif occupation == 'insuranceUnderwriters':
+        dag_matrix.loc['"Sink"'] = [0] * n
+        dag_matrix['"Sink"'] = [1, 0, 0, 1, 0, 1, 0, 0]
+    
+    return dag_matrix 
+
+
+
+# For HTML graph positions
+def node_positions(occupation):
+    if occupation == 'travelAgents':
+        fixed_positions = {
+        'Collect payment for transportation and accommodations from customer.': 
+        (0, -100),
+        'Converse with customer to determine destination, mode of transportation, travel dates, financial considerations, and accommodations required.': 
+        (-400, 200),
+        "Compute cost of travel and accommodations, using calculator, computer, carrier tariff books, and hotel rate books, or quote package tour's costs.": 
+        (-200, -100),
+        'Book transportation and hotel reservations, using computer or telephone.': 
+        (100, 0),
+        'Plan, describe, arrange, and sell itinerary tour packages and promotional travel incentives offered by various travel carriers.':
+        (-300, 0),
+        'Provide customer with brochures and publications containing travel information, such as local customs, points of interest, or foreign country regulations.':
+        (-400, -200),
+        'Print or request transportation carrier tickets, using computer printer system or system link to travel carrier.':
+        (200, 200),
+        'Record and maintain information on clients, vendors, and travel packages.':
+        (200, -200),
+        '"Sink"':
+        (300, 0)
+        }
+    elif occupation == 'insuranceUnderwriters':
+        fixed_positions = {
+        'Decline excessive risks.': 
+        (500, -150),
+        'Write to field representatives, medical personnel, or others to obtain further information, quote rates, or explain company underwriting policies.': 
+        (0, 0),
+        'Evaluate possibility of losses due to catastrophe or excessive insurance.': 
+        (200, 300),
+        'Decrease value of policy when risk is substandard and specify applicable endorsements or apply rating to ensure safe, profitable distribution of risks, using reference materials.': 
+        (300, 0),
+        'Review company records to determine amount of insurance in force on single risk or group of closely related risks.':
+        (200, -300),
+        'Authorize reinsurance of policy when risk is high.':
+        (500, 150),
+        'Examine documents to determine degree of risk from factors such as applicant health, financial standing and value, and condition of property.':
+        (-200, 0),
+        '"Sink"':
+        (600, 0)
+        }
+    return fixed_positions
+
+
+# Plotting interactive graph - two functions: compare_graphs and plot_graphs
+def compare_graphs(df1, df2):
+    '''
+    For getting common elements of both graphs + unique elements of each graph
+    '''
+    # Find common rows based on columns "scenario.task_A" and "scenario.task_B"
+    common = pd.merge(df1, df2, 
+                    on=['source', 'target'], 
+                    suffixes=('_df1', '_df2'))
+
+    # Find unique rows in w
+    merged = pd.merge(df1, common[['source', 'target']], 
+                    on=['source', 'target'], 
+                    how='outer', 
+                    indicator=True)
+    in1_notIn2 = merged[merged['_merge'] == 'left_only'].drop(columns=['_merge'])
+
+
+    # Find unique rows in wo
+    merged = pd.merge(df2, common[['source', 'target']], 
+                    on=['source', 'target'], 
+                    how='outer', 
+                    indicator=True)
+
+    # Keep only rows that are in df1 but not in df2
+    in2_notIn1 = merged[merged['_merge'] == 'left_only'].drop(columns=['_merge'])
+
+    return common, in1_notIn2, in2_notIn1
+
+
+def insert_line_breaks(text, max_length=80):
+    '''
+    For breaking long GPT comment lines 
+    '''
+    words = text.split()
+    lines = []
+    current_line = []
+    current_length = 0
+    
+    for word in words:
+        if current_length + len(word) + 1 > max_length:
+            lines.append(" ".join(current_line))
+            current_line = [word]
+            current_length = len(word) + 1
+        else:
+            current_line.append(word)
+            current_length += len(word) + 1
+    
+    if current_line:
+        lines.append(" ".join(current_line))
+    
+    return "\n".join(lines)
+
+
+def plot_graphs(occupation, 
+                df1, df2, 
+                df1_comment, 
+                df2_comment,
+                df1_unique_color, df2_unique_color, 
+                save_plot):
+    # Compare two graphs
+    common, unique_to_df1, unique_to_df2 = compare_graphs(df1, df2)
+
+    # Break down long comments into multiple lines
+    unique_to_df1['comment'] = unique_to_df1['comment'].apply(lambda x: insert_line_breaks(x))
+    unique_to_df2['comment'] = unique_to_df2['comment'].apply(lambda x: insert_line_breaks(x))
+
+    # Create a Pyvis network
+    net = Network(notebook=True, directed=True, cdn_resources="remote",
+                height = "800px",
+                    width = "125%",
+                    select_menu = True,
+                    filter_menu = True,)
+
+    # Add nodes with fixed positions and labels
+    fixed_positions = node_positions(occupation)
+    for node, (x, y) in fixed_positions.items():
+        net.add_node(node, label=node.split(" ")[0], title=node, x=x, y=y, fixed=True, borderWidthSelected=10)
+
+    # Add edges with corresponding labels
+    for index, row in common.iterrows():
+        net.add_edge(row['source'], row['target'], title='In both', color='grey')
+    for index, row in unique_to_df1.iterrows():
+        net.add_edge(row['source'], row['target'], title=df1_comment + row['comment'], color=df1_unique_color)
+    for index, row in unique_to_df2.iterrows():
+        net.add_edge(row['source'], row['target'], title=df2_comment + row['comment'], color=df2_unique_color)
+
+    # Save interactive graph
+    net.save_graph(save_plot)
