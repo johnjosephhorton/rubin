af07f3474ea9799c2c9f135be3da9e0bd33c808d
John Horton <john.joseph.horton@gmail.com>
2025-01-16 06:47:41 -0500

    Up to date model

---
+++ analysis/create_horizon_effects.py
@@ -0,0 +1,17 @@
+from CostVisualization import CostVisualization
+
+# Create visualization instance
+viz = CostVisualization(c_m=1.0, c_h=3)
+
+starting_points = [(0.4, 0.2), (0.4, 0.19), (0.4, 0.18), (0.4, 0.17)]
+
+time_horizons = [2 * x for x in [20, 40, 50, 80]]
+
+# Plot all paths with different time horizons
+viz.plot_optimal_paths(starting_points, time_horizons, m=200)
+
+# Plot all paths
+# viz.plot_optimal_paths(starting_points, T=150, m=100)
+
+# Save the result
+viz.save_figure("../writeup/plots/horizon_effects.pdf")
---
+++ analysis/create_optimal_paths.py
@@ -0,0 +1,27 @@
+from CostVisualization import CostVisualization
+
+m = 300
+
+# Create visualization instance
+viz = CostVisualization(c_m=1.0, c_h=3)
+
+starting_points = [
+    (0.1, 0.9),
+    (0.1, 0.1),  # Point 1
+    (0.3, 0.2),  # Point 2
+    (0.2, 0.4),  # Point 3
+    (0.4, 0.3),  # Point 4
+    (0.7, 0.6),
+    (0.7, 0.2),
+]
+
+time_horizons = [int(m / 100) * x for x in [100] * len(starting_points)]
+
+# Plot all paths with different time horizons
+viz.plot_optimal_paths(starting_points, time_horizons, m=m)
+
+# Plot all paths
+# viz.plot_optimal_paths(starting_points, T=150, m=100)
+
+# Save the result
+viz.save_figure("../writeup/plots/optimal_paths.pdf")
